1b9a550c5d6fff0f09279fb8796c5373deda0597
luciaserrano <lucia.serrano@escuelaestech.es>
2021-12-15 23:17:20 +0100

    añadir root

--- Config/db.php
+++ Config/db.php
@@ -10,7 +10,7 @@ class Database
     public static function getBdd() {
         try{
             if(is_null(self::$bdd)) {
-                self::$bdd = new PDO("mysql:host=localhost;dbname=proyectomvc", 'root');
+                self::$bdd = new PDO("mysql:host=localhost;dbname=proyectomvc", 'root', 'root');
             }
             return self::$bdd;
         }
